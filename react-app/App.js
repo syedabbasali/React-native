2984d8a982ac0130d550a255b3846740b86e896d
syedabbasali <28565154+syedabbasali@users.noreply.github.com>
2023-03-09 18:15:52 +0000

    Text changes

--- react-app/App.js
+++ react-app/App.js
@@ -10,7 +10,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Enter name: </Text>
+      <Text>Enter Your Name: </Text>
       <TextInput 
       multiline
 

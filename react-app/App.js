a10d0609cb82658e64eb485edcfe9d1613086e91
syedabbasali <28565154+syedabbasali@users.noreply.github.com>
2023-03-10 13:14:06 +0000

    To Do App

--- react-app/App.js
+++ react-app/App.js
@@ -1,47 +1,52 @@
-import React,{useState} from 'react';
-import { StatusBar  } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, ScrollView,FlatList } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  FlatList,
+  Touchable,
+} from "react-native";
+import { TouchableOpacity } from "react-native-web";
 // import { Button } from 'react-native-web';
 
 export default function App() {
-  const [people, setPeople] = useState ([
-    { name: 'Sultan', id:'1'  },
-    { name: 'ibrahim', id:'2' },
-    { name: 'Syed', id:'3'    },
-    { name: 'Sakib', id:'4'   },
-    { name: 'Sulla',id:'5'   },
-    { name: 'Majid', id:'6'   },
-    { name: 'Aayush',id:'7'  },
-    { name: 'Sahil', id:'8'   },
-    { name: 'Quatib', id:'9'   },
-    { name: 'Abhi', id:'9'   },
-    ]);
-  // const [age, setAge] = useState('30');
+  const [people, setPeople] = useState([
+    { name: "Sultan", id: "1" },
+    { name: "ibrahim", id: "2" },
+    { name: "Syed", id: "3" },
+    { name: "Sakib", id: "4" },
+    { name: "Sulla", id: "5" },
+    { name: "Majid", id: "6" },
+    { name: "Aayush", id: "7" },
+    { name: "Sahil", id: "8" },
+    { name: "Quatib", id: "9" },
+    { name: "Abhi", id: "10" },
+    { name: "Abhi", id: "11" },
+
+  ]);
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id !=id)
+    })
+  }
 
   return (
     <View style={styles.container}>
-<FlatList
-numColumns={5}
-keyExtractor={(item) => item.id}
-
-data={people}
-renderItem={({ item}) => (
-  <Text style={styles.item}>{item.name}</Text> 
-)}
-/>
-
-
-
-
-
-      {/* <ScrollView>
-      { people.map(item=> (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text> 
-          </View>
-        ) 
-      )}
-      </ScrollView> */}
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+        <Text style={styles.item}>{item.name}</Text>
+        </TouchableOpacity>
+  )}
+      />
     </View>
   );
 }
@@ -49,19 +54,18 @@ renderItem={({ item}) => (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop:40,
-    paddingHorizontal:20
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingHorizontal: 20,
     // alignItems: 'center',
     // justifyContent: 'center',
   },
-  item : {
+  item: {
     marginTop: 24,
     padding: 30,
-    backgroundColor: 'pink',
-    fontSize :24,
-    marginHorizontal:10,
-    marginTop:24,
-  }
+    backgroundColor: "pink",
+    fontSize: 24,
+    marginHorizontal: 10,
+   
+  },
 });
-

c0d5c87220ade2a99827e6bca28b0a8e3cf201fc
syedabbasali <28565154+syedabbasali@users.noreply.github.com>
2023-03-14 12:45:42 +0000

    ToDo App is ready

---
+++ Todo App/Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Syed Abbas Ali on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- react-app/App.js
+++ react-app/App.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import {
   StyleSheet,Text, View,FlatList } from "react-native";
 import Header from "./components/header";
-
+import TodoItem from "./components/todoitem";
+import AddTodo from "./components/addTodo";
 
 
 export default function App()  {
@@ -12,22 +13,38 @@ const [todos, setTodos] = useState([
   {text : 'play football', key : '3' },
 ]);
 
+
+const pressHandler = (key) => {
+setTodos((prevTodos) => {
+  return prevTodos.filter(todo => todo.key != key);
+}) ;
+}
+
+const submitHandler = (text) => {
+  setTodos((prevTodos) => {
+return [
+  { text: text, key: Math.random().toString() },
+  ...prevTodos
+];
+  })
+}
+
   return (
     <View style={styles.container}>
       <Header />
      <View style ={styles.content}>
-      {/* To-do form */}
+      <AddTodo submitHandler ={submitHandler} />
       <View style= {styles.list}>
         <FlatList
         data={todos}
         renderItem = {({ item }) =>(
-          <Text>{item.text}</Text>
+          <TodoItem item={item} pressHandler ={pressHandler} />
         )}
        />
       </View>
      </View>
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
